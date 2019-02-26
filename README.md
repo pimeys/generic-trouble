a5e8f50341eeb0c310b50234e62b3427f0461ee0
Julius de Bruijn <bruijn@prisma.io>
2019-02-26 11:51:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Prisma Rust Quiz #4
+
+This time we have a simple database project we want to test. Whenever we have
+exactly the same datasets, we want to be able to say they are the same set.
+
+The dataset contains `Itemizable` items, implementing the trait and returning
+the correct foreign key.
+
+In the example the test to compare two datasets doesn't compile. Investigate
+different ways of solving the problem without losing the original functionality.
+
+There can be many different solutions, all discussion is welcome.
+
+## Running the tests
+
+``` sh
+cargo test
+```
---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+pub trait Itemizable {
+    fn foreign_key(&self) -> usize;
+}
+
+#[derive(Default)]
+pub struct Dataset {
+    pub name: &'static str,
+    pub items: Vec<Box<dyn Itemizable>>,
+}
+
+impl Dataset {
+    pub fn new(name: &'static str) -> Self {
+        Dataset {
+            name,
+            ..Default::default()
+        }
+    }
+}
+
+pub struct DogItem {
+    pub name: &'static str,
+    pub age: u8,
+}
+
+impl DogItem {
+    pub fn new(name: &'static str, age: u8) -> Self {
+        DogItem { name, age }
+    }
+}
+
+impl Itemizable for DogItem {
+    fn foreign_key(&self) -> usize {
+        (self.age * 2) as usize
+    }
+}
+
+pub struct CatItem {
+    pub name: &'static str,
+    pub meows: u16,
+}
+
+impl CatItem {
+    pub fn new(name: &'static str, meows: u16) -> Self {
+        CatItem { name, meows }
+    }
+}
+
+impl Itemizable for CatItem {
+    fn foreign_key(&self) -> usize {
+        (self.meows / 2) as usize
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn two_datasets_are_the_same() {
+        let mut dataset1 = Dataset::new("Cats and dogs");
+        dataset1.items.push(Box::new(DogItem::new("woofie", 4)));
+        dataset1.items.push(Box::new(CatItem::new("woofie", 10000)));
+
+        let mut dataset2 = Dataset::new("Cats and dogs");
+        dataset2.items.push(Box::new(DogItem::new("woofie", 4)));
+        dataset2.items.push(Box::new(CatItem::new("woofie", 10000)));
+
+        assert_eq!(dataset1, dataset2);
+    }
+}
